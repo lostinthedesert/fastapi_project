23b8647bc5fd46ef22bf18794115eac7e9161e33
nick <cnickm@gmail.com>
2022-06-17 20:57:38 -0700

    added --port to procfile, changed port type to int in config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     database_hostname: str
-    database_port: int
+    database_port: str
     database_password: str
     database_name: str
     database_username: str
