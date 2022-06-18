6665b0e9bccd5fd3b0afde0419cf3393ff73d882
nick <cnickm@gmail.com>
2022-06-17 18:44:03 -0700

    commit changes to ignore /notations

--- app/notations/utils_notated.py
+++
@@ -1,9 +0,0 @@
-# Here is our self created password hashing module that includes importing passlib. All of this is copy/paste code from the passlib
-# docs, including the below function 'hash' which will perform the hashing action for our user's password.
-
-from passlib.context import CryptContext
-
-pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash(password: str):
-    return pwd_context.hash(password)
