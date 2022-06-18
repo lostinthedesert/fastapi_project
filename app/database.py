372dd07f15405956f7fefbd8fb26895a4af6743d
nick <cnickm@gmail.com>
2022-06-17 20:25:59 -0700

    added port number variable to database.py

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from .config import settings
 #from psycopg2.extras import RealDictCursor
 
 # connection string
-SQLALCHEMY_DATABASE_URL =f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL =f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 #engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL) 
