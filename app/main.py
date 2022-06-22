d34d7ae53abf62f16af34fd28a0e3781c5a60281
nick <cnickm@gmail.com>
2022-06-21 20:18:40 -0700

    changed welcome message

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 # Root path
 @app.get("/") 
 def root():
-    return {"message": "this message will automatically reload :)"}
+    return {"message": "this is the official pando llc homepage :)"}
